9b428487e02f2cff285a7a30c58fe4d81a2f3b18
luckyanand235 <luckyanand235@gmail.com>
2023-08-15 12:39:17 +0530

    formatting

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,29 +1,26 @@
-import { Component, EventEmitter, Output } from "@angular/core";
-import { Post } from "../posts.model";
-import { NgForm } from "@angular/forms";
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Post } from '../posts.model';
+import { NgForm } from '@angular/forms';
 // Decorator to make it angular component
 
 @Component({
-	selector: 'app-post-create',
-	templateUrl: './post-create.component.html',
-	styleUrls: [
-		'./post-create.component.css'
-	]
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
 })
-
 export class PostCreateComponent {
   enteredContent = '';
-	enteredTitle = '';
-  @Output()	postCreated = new EventEmitter<Post>();
+  enteredTitle = '';
+  @Output() postCreated = new EventEmitter<Post>();
 
-	onAddPost(form: NgForm) {
-		if(form.invalid) {
-			return;
-		}
-		const post: Post = {
-			title: form.value.title, 
-			content: form.value.content
-		};
-		this.postCreated.emit(post);
-	}
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content,
+    };
+    this.postCreated.emit(post);
+  }
 }
