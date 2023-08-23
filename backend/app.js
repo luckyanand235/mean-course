ac873f5a3418daa6c9664949e01dd003c78eaef1
luckyanand235 <luckyanand235@gmail.com>
2023-08-23 22:29:04 +0530

    #get post api added

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,24 @@ var express = require('express');
 const app = express();
 
 
-app.use((req, res, next) => {
-    console.log("First Middleware");
-    next();
-});
 
-app.use((req, res, next) => {
-    res.send("Hellp from express!");
+app.use('/api/posts' ,(req, res, next) => {
+	const posts = [
+		{ 
+			id: 'asg3444gassd',
+			title: 'First server side post',
+			content: 'This is the first post coming from server.'
+		},
+		{
+			id: 'hrtheg4g5465',
+			title: 'Second server-side post',
+			content: 'This is the second post coming from express server.'
+		}
+	];
+	res.status(200).json({
+		message: 'Posts feteced successfully!',
+		posts: posts
+	});
 });
 
 module.exports = app;
